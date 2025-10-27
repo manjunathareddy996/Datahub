35b04f08586a935f4ef6d957c376f547798d03f6
Ronith Kumar <Ronith.Kumar@bajajallianz.co.in>
2025-10-27 12:24:20 +0530

    Dev workflows added

---
+++ SQL_DIR/snowsql-1.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE PROCEDURE demo14444("INPUT" VARCHAR(16777216))
+RETURNS VARCHAR(1736)
+LANGUAGE SQL
+EXECUTE AS CALLER
+AS '
+DECLARE out VARCHAR;
+BEGIN
+    -- using for tracking
+    out := ''Your Input parameter from  : '' || input;
+    RETURN out;
+END;
+';
+
+ 
