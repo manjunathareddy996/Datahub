ecd8ce30b992c41cddfc05a61dd9eb24a13a1fbf
ashrafshaikengr <ashrafshaik.engr@gmail.com>
2026-08-19 13:19:23 +0530

    get_incremental macro added

--- demo/stg_a.sql
+++ demo/stg_a.sql
@@ -8,3 +8,4 @@ select
     nullif(trim(to_varchar(phone_1)), '')       as phone_1,
     updated_at                                   as updated_at
 from {{ source('demo_raw', 'TABLE_A') }}
+{{ get_incremental_filter('BAGIC_PREPROD_CURATED_DB', 'BGIL_DEV_DATA_MODEL', 'SAT_A_B', 'TABLE_A') }}
